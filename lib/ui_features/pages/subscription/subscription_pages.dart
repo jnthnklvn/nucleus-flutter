476d6546baf33d17389df58ef8cc1cc0702f6f10
Jonathan Kelvin <jnthn.klvn.s@gmail.com>
2023-11-07 00:55:53 -0300

    fix: fixes overflow on subscriptions bottom page

--- lib/ui_features/pages/subscription/subscription_pages.dart
+++ lib/ui_features/pages/subscription/subscription_pages.dart
@@ -22,105 +22,108 @@ class SubScriptionPages extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            child: const Text(
-              'Get unlimited access to our programs.',
-              style: AssetStyles.t3,
-              textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              child: const Text(
+                'Get unlimited access to our programs.',
+                style: AssetStyles.t3,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          verticalSpace(30),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            child: const Text(
-              'Take the first step towards a healthier and\nhappier life.',
-              style: AssetStyles.labelSmReguler,
-              textAlign: TextAlign.center,
+            verticalSpace(30),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              child: const Text(
+                'Take the first step towards a healthier and\nhappier life.',
+                style: AssetStyles.labelSmReguler,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          verticalSpace(20),
-          CarouselSlider(
-            options: CarouselOptions(
-              disableCenter: true,
-              enlargeCenterPage: true,
-              aspectRatio: 1.1,
-              height: 352,
-              enableInfiniteScroll: false,
-            ),
-            items: [1, 2, 3].map((e) {
-              return Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 30,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AssetColors.skyWhite,
-                ),
-                child: Column(
-                  children: [
-                    const Text(
-                      'POPULAR',
-                      style: AssetStyles.labelTinyReguler,
-                    ),
-                    verticalSpace(15),
-                    Text(
-                      'Exercise Class',
-                      style: AssetStyles.labelLgRegular.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    verticalSpace(15),
-                    const Text(
-                      '\$60.99',
-                      style: AssetStyles.t1,
-                    ),
-                    verticalSpace(15),
-                    const Text(
-                      'For 1 Year',
-                      style: AssetStyles.labelTinyReguler,
-                    ),
-                    const Spacer(),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 50),
-                      child: const Text(
-                        'iOS, Android, Apple TV, Roku,'
-                        '\nAmazon Fire TV, web browser',
+            verticalSpace(20),
+            CarouselSlider(
+              options: CarouselOptions(
+                disableCenter: true,
+                enlargeCenterPage: true,
+                aspectRatio: 1.1,
+                height: 352,
+                enableInfiniteScroll: false,
+              ),
+              items: [1, 2, 3].map((e) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: AssetColors.skyWhite,
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'POPULAR',
                         style: AssetStyles.labelTinyReguler,
-                        textAlign: TextAlign.center,
                       ),
-                    ),
-                    verticalSpace(20),
-                    ButtonPrimary(
-                      onTap: () {
-                        debugPrint('asd');
-                      },
-                      width: screenWidth(context),
-                      color: AssetColors.primaryBase,
-                      text: 'Subscribe',
-                      height: 50,
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          verticalSpace(20),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            child: const Text(
-              'You will be charged \$9.99 (monthly plan) or \$60.99'
-              '\n(annual plan) through your iTunes account. You can'
-              '\ncancel at any time if your not satisfied.',
-              style: AssetStyles.labelTinyReguler,
-              textAlign: TextAlign.center,
+                      verticalSpace(15),
+                      Text(
+                        'Exercise Class',
+                        style: AssetStyles.labelLgRegular.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      verticalSpace(15),
+                      const Text(
+                        '\$60.99',
+                        style: AssetStyles.t1,
+                      ),
+                      verticalSpace(15),
+                      const Text(
+                        'For 1 Year',
+                        style: AssetStyles.labelTinyReguler,
+                      ),
+                      const Spacer(),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        child: const Text(
+                          'iOS, Android, Apple TV, Roku,'
+                          '\nAmazon Fire TV, web browser',
+                          style: AssetStyles.labelTinyReguler,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      verticalSpace(20),
+                      ButtonPrimary(
+                        onTap: () {
+                          debugPrint('asd');
+                        },
+                        width: screenWidth(context),
+                        color: AssetColors.primaryBase,
+                        text: 'Subscribe',
+                        height: 50,
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
-          ),
-        ],
+            verticalSpace(20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              child: const Text(
+                'You will be charged \$9.99 (monthly plan) or \$60.99'
+                '\n(annual plan) through your iTunes account. You can'
+                '\ncancel at any time if your not satisfied.',
+                style: AssetStyles.labelTinyReguler,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            verticalSpace(32),
+          ],
+        ),
       ),
     );
   }
